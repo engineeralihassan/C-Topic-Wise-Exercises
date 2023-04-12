91357676978d5d3efdaaeec88a9e795dd40e3a3b
Ali Hassan <engineralihassan@gmail.com>
2023-04-11 17:38:20 -0700

    Add project files.

---
+++ 01-Basic_programs/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01_Basic_programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to C# Basic Programs");
+            Console.WriteLine("Program # 01");
+
+            Console.WriteLine("Enter your testing number");
+            int num = int.Parse(Console.ReadLine());
+            if(num %2 == 0)
+            {
+                Console.WriteLine("The number is Even");
+            }
+            else
+            {
+                Console.WriteLine("The Number is Odd");
+            }
+        }
+    }
+}
---
+++ C# Topic Wise Exercises/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__Topic_Wise_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("In this Section we start to solve the topic wise C# exercise per day ");
+        }
+    }
+}
