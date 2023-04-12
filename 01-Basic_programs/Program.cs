7eb138826aaf88041195efd5d7287bdd228ab1e2
Ali Hassan <engineralihassan@gmail.com>
2023-04-11 17:48:38 -0700

    Programe no 2

--- 01-Basic_programs/Program.cs
+++ 01-Basic_programs/Program.cs
@@ -5,18 +5,29 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to C# Basic Programs");
-            Console.WriteLine("Program # 01");
+            //Console.WriteLine("Program # 01");
 
-            Console.WriteLine("Enter your testing number");
-            int num = int.Parse(Console.ReadLine());
-            if(num %2 == 0)
+            //Console.WriteLine("Enter your testing number");
+            //int num = int.Parse(Console.ReadLine());
+            //if(num %2 == 0)
+            //{
+            //    Console.WriteLine("The number is Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The Number is Odd");
+            //}
+            // Programe number 2
+            Console.WriteLine("Programe # 2");
+            IEnumerable<int> oddNums =
+
+    Enumerable.Range(20, 10).Where(x => x % 2 != 0);
+
+            foreach (int n in oddNums)
             {
-                Console.WriteLine("The number is Even");
+                Console.WriteLine(n);
             }
-            else
-            {
-                Console.WriteLine("The Number is Odd");
-            }
+           
         }
     }
 }
